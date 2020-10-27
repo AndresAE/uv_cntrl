77cda01347848d4c5a0b751da3b7034f8d977abf
AndresSandoval8 <48926398+AndresSandoval8@users.noreply.github.com>
2020-10-27 16:22:40 -0700

    WiP filters

--- src/common/filters/Filters.cpp
+++ src/common/filters/Filters.cpp
@@ -3,90 +3,64 @@
   Copyright (c) 2020 Andres Sandoval.  All right reserved.
 */
 
-// include core Wiring API
-#include "WProgram.h"
-
 // include this library's description file
 #include "Filters.h"
+int n = 1;
 
 // Constructor /////////////////////////////////////////////////////////////////
 // Function that handles the creation and setup of instances
 
-Filters::Filters(int givenValue)
+Filters::Filters()
 {
 	// initialize this instance's variables
-	value = givenValue;
-
-	// do whatever is required to initialize the library
-	pinMode(13, OUTPUT);
-	Serial.begin(9600);
 }
 
 // Public Methods //////////////////////////////////////////////////////////////
 // Functions available in Wiring sketches, this library, and other libraries
 
-void Filters::complementary(void)
+float Filters::derivative(double y, float dt)
 {
 	// eventhough this function is public, it can access
 	// and modify this library's private variables
-	Serial.print("value is ");
-	Serial.println(value);
+	float dydt = (y[n] - y[n - 1]) / dt;
+	return dydt;
 
 }
 
-void Filters::derivative(void)
+float Filters::high_pass(double x, double y, float hp_frequency, float dt)
+{
+	// eventhough this function is public, it can access
+	float alpha = (1 / hp_frequency) / ((1 / hp_frequency) + dt);
+	float y_filtered = alpha * (y[n - 1] + x[n] - x[n - 1]);
+	return y_filtered;
+
+}
+
+float Filters::integral(double y, float dt, float c)
 {
 	// eventhough this function is public, it can access
 	// and modify this library's private variables
-	dydt = (y[n] - y[n-1]) / dt
+	float inty = (y[n] - y[n - 1]) * dt / 2 + c;
+	return inty;
 
 }
 
-void Filters::integral(void)
+float Filters::limit(float y, float limits)
 {
 	// eventhough this function is public, it can access
 	// and modify this library's private variables
-	dydt = (y[n] - y[n-1]) * dt / 2 + c
+	if (y < limits[n - 1]) { y = limits[n - 1]; }
+	if (y > limits[n]) { y = limits[n]; }
+	return y;
 
 }
 
-void Filters::high_pass(void)
-{
-	// eventhough this function is public, it can access
-	alpha = (1 / hp_frequency) / ((1 / hp_frequency) + dt);
-	y[n] = alpha * (y[n - 1] + x[n] - x[n - 1]);
-	x[n - 1] = x[n];
-	y[n - 1] = y[n];
-
-}
-
-void Filters::limit(void)
+float Filters::low_pass(float x, double y, float hp_frequency, float dt)
 {
 	// eventhough this function is public, it can access
 	// and modify this library's private variables
-	if (y[n] < limits[0]) { y[n] = limits[0]; }
-	if (y[n] > limits[1]) { y[n] = limits[1]; }
+	float alpha = dt / ((1 / lp_frequency) + dt);
+	float y_filtered = alpha * x + (1 - alpha) * y[n - 1];
+	return y_filtered;
 
 }
-
-void Filters::low_pass(void)
-{
-	// eventhough this function is public, it can access
-	// and modify this library's private variables
-	alpha = dt / ((1 / lp_frequency) + dt);
-	y[n] = alpha * x + (1 - alpha) * y[n - 1];
-	y[n - 1] = y[n];
-
-}
-
-// Private Methods /////////////////////////////////////////////////////////////
-// Functions only available to other functions in this library
-
-void Filters::doSomethingSecret(void)
-{
-	digitalWrite(13, HIGH);
-	delay(200);
-	digitalWrite(13, LOW);
-	delay(200);
-}
-
--- src/common/filters/Filters.h
+++ src/common/filters/Filters.h
@@ -8,25 +8,28 @@
 #define Filters_h
 
 // include types & constants of Wiring core API
-#include "WConstants.h"
+#if (ARDUINO >=100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
 
 // library interface description
 class Filters
 {
     // user-accessible "public" interface
 public:
-    Filters(int);
-    void complementary(void);
-    void derivative(void);
-    void integral(void);
-    void high_pass(void);
-    void high_pass(void);
+    // constructor
+    Filters();
 
-    // library-accessible "private" interface
+    // methods
+    float derivative(double y, float dt);
+    float high_pass(double x, double y, float hp_frequency, float dt);
+    float integral(double y, float dt, float c = 0);
+    float limit(float y, float limits);
+    float low_pass(double x, double y, float hp_frequency, float dt);
+
 private:
-    int value;
-    void doSomethingSecret(void);
 };
 
 #endif
-
