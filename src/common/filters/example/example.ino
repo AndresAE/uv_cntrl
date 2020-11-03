ecafe7aedc990e5127d22eed443ab04ef16496e0
AndresSandoval8 <48926398+AndresSandoval8@users.noreply.github.com>
2020-11-03 15:29:45 -0800

    Corrected filter inputs

--- src/common/filters/example/example.ino
+++ src/common/filters/example/example.ino
@@ -2,9 +2,10 @@
 #include <math.h>
 Filters Filters;
 
-float y [] = {0, 0}; float y_filtered = 0;
+float y [] = {0, 0}; 
+float y_filtered [] = {0, 0};
 float x = 0;
-float lp_frequency = 0.5;
+float lp_frequency = 100;
 float t0 = millis();
 float t, t1 = 0, t2, dt;
 float pi = 3.14;
@@ -19,10 +20,11 @@ void loop() {
   t2 = millis();
   t = t2 - t0;
   dt = t2 - t1;
-  y[1] = sin (t * 3 * pi / 1000);
-  y_filtered = Filters.low_pass(x, y, lp_frequency, dt / 1000);
+  y[1] = sin (t * 2 * pi / 1000);
+  y_filtered[1] = Filters.low_pass(y[0], y_filtered, lp_frequency, dt / 1000);
   Serial.println(y[1]);
-  Serial.println(y_filtered);
+  Serial.println(y_filtered[1]);
   y[0] = y[1];
+  y_filtered[0] = y_filtered[1];
   t1 = t2;
 }
