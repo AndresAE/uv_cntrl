78f003bfc7a6b2ea170545f5070700b566665bf3
AndresSandoval8 <48926398+AndresSandoval8@users.noreply.github.com>
2020-11-12 20:23:03 -0800

    added performance calcs

--- src/libraries/FlightLib/FlightLib.cpp
+++ src/libraries/FlightLib/FlightLib.cpp
@@ -9,28 +9,49 @@
 // Constructor /////////////////////////////////////////////////////////////////
 // Function that handles the creation and setup of instances
 
-FlightLib::FlightLib( float g=32.172 )
+FlightLib::FlightLib( float g=32.172, float rho=0.00238 )
 {
 	_g = g;
+	_rho = rho;
 }
 
 // Public Methods //////////////////////////////////////////////////////////////
-float FlightLib::alpha_est(float n_z, float v, float w, float s, float rho=0.00238, float c_l_a=2*M_PI )
+float FlightLib::alpha_est( float n_z, float v, float w, float s, float c_l_a=2*M_PI )
 {
-	float alpha = 2 * w * n_z / (rho * pow(v, 2) * s * c_l_a) * 180 / M_PI;
+	float alpha = 2 * w * n_z / (_rho * pow(v, 2) * s * c_l_a) * 180 / M_PI;
 }
 
-float FlightLib::az2nz(float a_z)
+float FlightLib::ax2nx( float a_x )
+{
+	float n_x = a_x / _g;
+}
+
+float FlightLib::ay2ny( float a_y )
+{
+	float n_y = a_y / _g;
+}
+
+float FlightLib::az2nz( float a_z )
 {
 	float n_z = a_z / _g + 1;
 }
 
-float FlightLib::nz2az(float n_z)
+float FlightLib::nx2ax( float n_x )
+{
+	float a_x = _g * n_x;
+}
+
+float FlightLib::ny2ay( float n_y )
+{
+	float a_z = _g * n_y;
+}
+
+float FlightLib::nz2az( float n_z )
 {
 	float a_z = _g * (n_z - 1);
 }
 
-float FlightLib::nz2phi(float n_z)
+float FlightLib::nz2phi( float n_z )
 {
 	float phi = acos(1 / n_z) * 180 / M_PI;
 }
@@ -38,4 +59,19 @@ float FlightLib::nz2phi(float n_z)
 float FlightLib::phi2nz( float phi )
 {
 	float n_z = 1 / cos (phi * M_PI / 180);
+}
+
+float FlightLib::v_gamma( float w, float n, float s, float ar, float c_d_0=0.05 )
+{
+	float v = sqrt(2 * w * n / (_rho * s * sqrt(c_d_0 * M_PI * ar)));
+}
+
+float FlightLib::v_s( float alpha, float gamma, float w, float n, float s, float c_l_a=2*M_PI )
+{
+	float v = sqrt(2 * w * n / (_rho * s * c_l_a * (alpha - gamma)));
+}
+
+float FlightLib::v_vy( float w, float n, float s, float ar, float c_d_0=0.05 )
+{
+	float v = sqrt(2 * w * n / (_rho * s * sqrt(3 * c_d_0 * M_PI * ar)));
 }
--- src/libraries/FlightLib/FlightLib.h
+++ src/libraries/FlightLib/FlightLib.h
@@ -20,16 +20,24 @@ class FlightLib
 {
     // user-accessible "public" interface
 public:
-    FlightLib( float g );
-    float alpha_est(float n, float v, float w, float s, float rho, float c_l_a);
+    FlightLib( float g, float rho );
+    float alpha_est( float n_z, float v, float w, float s, float c_l_a );
+    float ax2nx( float a_x );
+    float ay2ny( float a_y );
     float az2nz( float a_z );
+    float nx2ax(float n_x);
+    float ny2ay(float n_y);
     float nz2az( float n_z );
     float nz2phi( float n_z );
     float phi2nz( float phi );
+    float v_gamma( float w, float n, float s, float ar, float c_d_0 );
+    float v_s( float alpha, float gamma, float w, float n, float s, float c_l_a );
+    float v_vy( float w, float n, float s, float ar, float c_d_0 );
 
     // library-accessible "private" interface
 private:
     float _g;
+    float _rho;
 };
 
 #endif
